88d611c87441e572265784db37685eeef77421b0
Tur1ddu <orlandosalvatore294@gmail.com>
2025-07-08 17:33:28 +0200

    first commit

---
+++ esercizioConversionegradi.js
@@ -0,0 +1,9 @@
+/*let cacca = prompt("inserisci numero");
+let c = Number(cacca);
+let f = (c * 9/5) + 32;
+console.log(f);*/
+
+let cacca = prompt("inserisci numero");
+let f= Number(cacca);
+let c = (f - 32) * 5/9;
+console.log(c);
