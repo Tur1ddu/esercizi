954b6d8fe44611b31114fc2e8b8ee1c3f2aa6b3c
TristoM1etitore <alessiogelardi0601@gmail.com>
2025-07-13 02:46:01 +0200

    correzione quarto esercizio funzioni

--- eserciziofunzioni4.js
+++ eserciziofunzioni4.js
@@ -1,16 +1,20 @@
 console.log("------------TENTACOLATTE FUNZIONE 4------------");
 
 function unisciStringhe(stringa1, stringa2, separatore) {
-    unione = stringa1+separatore+stringa2;
+    let unione = stringa1+separatore+stringa2;
     return unione;
 }
 
-// Versione avanzata con numero variabile di parametri
+// Versione avanzata con numero variabile di parametri (non considerare)
 function unisciMultipleStringhe(...stringhe) {
-return stringhe;
+    let unione = "";
+    stringhe.forEach(stringa => {
+        unione += stringa
+    });
+    return unione;
 }
 
 // Test
 console.log(unisciStringhe("Ciao", "Mondo", " ")); // "Ciao Mondo"
-console.log(unisciMultipleStringhe("A", "B", "C", "D")); // "A B C D"
+console.log(unisciMultipleStringhe("A", "B", "C", "D")); // "ABCD"
 console.log("-------------PENSAVI DI AVER VINTO EBBENE---------------");
