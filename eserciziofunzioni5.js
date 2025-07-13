4e3d9b1e9b56cc381c30de41bd0594aab98f6a64
Tur1ddu <orlandosalvatore294@gmail.com>
2025-07-13 19:33:52 +0200

    Modifiche HTML + esercizio funzioni 5

---
+++ eserciziofunzioni5.js
@@ -0,0 +1,15 @@
+console.log("------------TENTACOLATTE FUNZIONE 5------------");
+
+function contaCarattere(testo, carattere) {
+  let count = 0;
+  for (let i = 0; i < testo.length; i++) {
+    if (testo[i] === carattere) {
+      count++;
+    }
+  }
+  return count;
+}
+// Test
+console.log(contaCarattere("javascript", "a")); // 2
+console.log(contaCarattere("hello world", "l")); // 3
+console.log("-------------PENSAVI DI AVER VINTO EBBENE---------------");
