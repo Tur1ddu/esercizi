8ab055e8a64f77abc9bb9d38b74eef8d8e9313f3
TristoM1etitore <alessiogelardi0601@gmail.com>
2025-07-13 02:51:03 +0200

    correzione primo esercizio funzioni

--- eserciziofunzioni2.js
+++ eserciziofunzioni2.js
@@ -1,15 +1,28 @@
 console.log("------------TENTACOLATTE FUNZIONE 2------------");
 function isPari(numero) {
+
+    /*
     let bol = true;
     let bul = false;
     if(numero%2 === 0){
-        numero = bol;
+        numero = bol; //4 = true (?) | 7 = true
     }else{
-       bol = bul;
+       bol = bul; //(?)
     }
-    return bol ;
+    return bol;
 
+    */
+
+    let result = false;
+
+    if(numero%2 === 0) {
+        result = true
+    }
+
+    return result;
 }
+
+
 console.log(isPari(4));
 console.log(isPari(7));
 console.log("-------------PENSAVI DI AVER VINTO EBBENE---------------");
