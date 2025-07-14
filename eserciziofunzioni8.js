d6c9821479635a7f57e2fb7a46124bf2930276cf
Tur1ddu <orlandosalvatore294@gmail.com>
2025-07-14 18:27:26 +0200

    ottavo esercizio + aggiunta collegamento

---
+++ eserciziofunzioni8.js
@@ -0,0 +1,21 @@
+console.log("------------TENTACOLATTE FUNZIONE 8------------");
+function somma(numeri1, numeri2){
+    let numeriSomma = 0;
+    if ("number" === typeof numeri1 && "number" ===  typeof numeri2){
+       numeriSomma = numeri1 + numeri2;
+    }else{
+        numeriSomma= "errore";
+    }
+    return numeriSomma;
+}
+
+
+
+
+
+
+console.log(somma(3, 7));       // 10
+console.log(somma(-2, 5));      // 3
+console.log(somma(3, "abc"));   // errore
+console.log(somma());           // errore
+console.log("-------------PENSAVI DI AVER VINTO EBBENE---------------");
