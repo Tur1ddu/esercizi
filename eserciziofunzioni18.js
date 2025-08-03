1c0c52686d4f59013de0cf785bfd10ce8f7724e0
Tur1ddu <orlandosalvatore294@gmail.com>
2025-08-03 18:19:14 +0200

    diciottesimo esercizio + modifiche html

--- eserciziofunzioni18.js
+++ eserciziofunzioni18.js
@@ -3,21 +3,21 @@ console.log("------------TENTACOLATTE FUNZIONE 18------------");
 
 function numeroRomano(n){
     let stringa = "";
-    
+    let risultato = "";
     let romani = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
     let numeri = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     if (n < 1 || n > 3999){
         stringa = "numero non valido";
     }else{
-        let risultato = "";
+        
         for (let i = 0; i < romani.length; i++ ){
-            
-            risultato+= romani[i];
-            n -= numeri[i];
-            
+            while (n >= numeri[i]) {
+                risultato += romani[i];
+                n -= numeri[i];
+            }
         }
     }
-    return n > 0 ? risultato : stringa;
+    return stringa.length > 0 ? stringa : risultato;
 }
 console.log(numeroRomano(27)); // "XXVII"
 console.log(numeroRomano(1994)); // "MCMXCIV"
