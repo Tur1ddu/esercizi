bcf9a87e9856dd8387b4670f1f127696bb4b2ec7
TristoM1etitore <alessiogelardi0601@gmail.com>
2025-08-04 14:59:28 +0200

    performance

--- eserciziofunzioni16.js
+++ eserciziofunzioni16.js
@@ -3,13 +3,13 @@ console.log("------------TENTACOLATTE FUNZIONE 16------------");
 
 function validaPIN(pin){
     let stringa = "";
-    let pinErrati = ["0000", "1234", "1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888", "9999"];
     if (Number.isNaN(Number(pin))){ // questa condizione verifica che il pin non è un numero
         stringa = "PIN deve contenere solo numeri";
     } else {
         if (pin.length < 4 || pin.length > 4){
             stringa = "pin deve essere di 4 cifre";
         }else{
+            let pinErrati = ["0000", "1234", "1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888", "9999"];
             for (let i = 0; i < pinErrati.length; i++){
                 if(pin === pinErrati[i]){
                     stringa = "pin troppo semplice";
